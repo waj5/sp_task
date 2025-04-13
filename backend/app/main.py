148a213099324874d5fa2576c5f043509e46c482
root <root@ser765999496602.local>
2025-04-13 04:44:25 +0000

    服务器版本

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,4 +34,4 @@ def read_root():
     return {"message": "Welcome to FastAPI backend!"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0",port=8000, reload=True)
